9b833d424f16c51760cb1ac2c0e4ce136b8cb6bc
bob lokerse <bob.lokerse@gmail.com>
2015-08-25 13:35:18 +0200

    Initial Solution

---
+++ OctoDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OctoDemo.Startup))]
+namespace OctoDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
