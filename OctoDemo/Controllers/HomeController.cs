bf7ff9e20289db9df72cef93bb4399e201f5cd82
bob lokerse <bob.lokerse@gmail.com>
2015-08-26 10:10:10 +0200

    Added a description in the About page.

--- OctoDemo/Controllers/HomeController.cs
+++ OctoDemo/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace OctoDemo.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "A demo application to show Octopus deploy in action.";
 
             return View();
         }
